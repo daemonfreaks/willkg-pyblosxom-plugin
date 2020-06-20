8ee773772a9aa0b872b2f487e1ff43fc6617b959
jun-g <daemonfreaks@gmail.com>
2020-06-20 18:02:30 +0700

    Python3 and PEP8 compliant.

--- plugins/wbgpager.py
+++ plugins/wbgpager.py
@@ -2,7 +2,7 @@
 Summary
 =======
 
-Plugin for paging long index pages.  
+Plugin for paging long index pages.
 
 PyBlosxom uses the num_entries configuration variable to prevent
 more than num_entries being rendered by cutting the list down
@@ -22,7 +22,7 @@ To install wbgpager, do the following:
    HTML will appear.
 
 
-Here are some additional configuration variables to adjust the 
+Here are some additional configuration variables to adjust the
 behavior::
 
   wbgpager_count_from
@@ -97,7 +97,7 @@ Revisions:
 1.5 - (26 September 2005) added configurable 1, 2, 3, 4, ... or Page 1 of 23
                           linking methodologies
 1.4 - (19 May 2005) added configurable next/prev links, additional spaceing
-                    between links, and configurable page start 
+                    between links, and configurable page start
                     (thanks Martin Michlmayr!)
 1.3 - (06 May 2005) fixed off-by-one issues (thanks Martin Michlmayr!)
 1.2 - (11 April 2005) fixed to work with PyBlosxom 1.2
@@ -107,22 +107,27 @@ Revisions:
 __author__ = "Will Kahn-Greene - willg at bluesock dot org"
 __version__ = "$Date$"
 __url__ = "https://github.com/willkg/pyblosxom-plugins"
-__description__ = "Allows navigation by page for indexes that have too many entries."
+__description__ = ("Allows navigation by page for indexes that have too many "
+                   "entries.")
+
 
 def verify_installation(request):
-    config = request.getConfiguration()
+    config = request.get_configuration()
     if config.get("num_entries", 0) == 0:
-        print "missing config property 'num_entries'.  wbgpager won't do "
-        print "anything without num_entries set.  either set num_entries "
-        print "to a positive integer, or disable the wbgpager plugin."
-        print "see the documentation at the top of the wbgpager plugin "
-        print "code file for more details."
-        return 0
+        print("missing config property 'num_entries'.  wbgpager won't do ")
+        print("anything without num_entries set.  either set num_entries ")
+        print("to a positive integer, or disable the wbgpager plugin.")
+        print("see the documentation at the top of the wbgpager plugin ")
+        print("code file for more details.")
+        return False
 
-    return 1
+    return True
+
 
 class PageDisplay:
-    def __init__(self, url, current_page, max_pages, count_from, previous, next, linkstyle):
+
+    def __init__(self, url, current_page, max_pages, count_from, previous,
+                 next, linkstyle):
         self._url = url
         self._current_page = current_page
         self._max_pages = max_pages
@@ -135,7 +140,7 @@ class PageDisplay:
         output = []
         # prev
         if self._current_page != self._count_from:
-            output.append('<a href="%s%d">%s</a>&nbsp;' % 
+            output.append('<a href="%s%d">%s</a>&nbsp;' %
                           (self._url, self._current_page - 1, self._previous))
 
         if self._linkstyle == 0:
@@ -146,25 +151,28 @@ class PageDisplay:
                     output.append('<a href="%s%d">%d</a>' %
                                   (self._url, i, i))
         elif self._linkstyle == 1:
-            output.append(' Page %s of %s ' % (self._current_page, self._max_pages-1))
+            output.append(' Page %s of %s ' % (self._current_page,
+                                               self._max_pages - 1))
 
         # next
         if self._current_page < self._max_pages - 1:
-            output.append('&nbsp;<a href="%s%d">%s</a>' % 
+            output.append('&nbsp;<a href="%s%d">%s</a>' %
                           (self._url, self._current_page + 1, self._next))
 
         return " ".join(output)
-    
+
+
 def page(request, num_entries, entry_list):
-    http = request.getHttp()
-    config = request.getConfiguration()
-    data = request.getData()
+    http = request.get_http()
+    config = request.get_configuration()
+    data = request.get_data()
 
     previous = config.get("wbgpager_previous_text", "&lt;&lt;")
     next = config.get("wbgpager_next_text", "&gt;&gt;")
 
     linkstyle = config.get("wbgpager_linkstyle", 1)
-    if linkstyle > 1: linkstyle = 1
+    if linkstyle > 1:
+        linkstyle = 1
 
     max = num_entries
     count_from = config.get("wbgpager_count_from", 0)
@@ -174,14 +182,14 @@ def page(request, num_entries, entry_list):
         form = http.get("form", None)
 
         # this is the new way to get the form (PyBlosxom 1.2 and after)
-        if not form and getattr(request, "getForm"):
-            form = request.getForm()
+        if not form and getattr(request, "get_form"):
+            form = request.get_form()
 
         page = count_from
         if form:
             try:
                 page = int(form.getvalue("page"))
-            except:
+            except (ValueError, TypeError):
                 page = count_from
 
         begin = (page - count_from) * max
@@ -207,64 +215,16 @@ def page(request, num_entries, entry_list):
 
         data["entry_list"] = entry_list[begin:end]
 
-        data["page_navigation"] = PageDisplay(url, page, maxpages, count_from, previous, next, linkstyle)
+        data["page_navigation"] = PageDisplay(url, page, maxpages, count_from,
+                                              previous, next, linkstyle)
 
     else:
         data["page_navigation"] = ""
 
 
-from Pyblosxom import pyblosxom
-
-########################################################################
-# This functionality handles PyBlosxom 1.5 by using the new truncatelist
-# callback.  No klugy num_entries sleight-of-hand anymore.
-
 def cb_truncatelist(args):
-    if pyblosxom.VERSION_SPLIT < (1, 5, 0):
-        return
-
     request = args["request"]
     entry_list = args["entry_list"]
 
     page(request, request.config.get("num_entries", 10), entry_list)
     return request.data.get("entry_list", entry_list)
-
-
-########################################################################
-# This functionality kicks in for pre PyBlosxom 1.5 versions that don't
-# have the truncatelist callback.  It's very klugy.
-
-def cb_start(args):
-    if pyblosxom.VERSION_SPLIT >= (1, 5, 0):
-        return
-
-    req = args["request"]
-    config = req.getConfiguration()
-
-    # we do a quick slight of hand here so that PyBlosxom doesn't
-    # go and cut down the list of entries before we get a chance
-    # to.
-    if not config.has_key("wbgpager_num_entries"):
-        ne = config.get("num_entries", 0)
-        config["wbgpager_num_entries"] = ne
-        config["num_entries"] = 0
-
-def cb_prepare(args):
-    if pyblosxom.VERSION_SPLIT >= (1, 5, 0):
-        return
-
-    request = args["request"]
-    http = request.getHttp()
-    config = request.getConfiguration()
-    data = request.getData()
-    previous = config.get("wbgpager_previous_text", "&lt;&lt;")
-    next = config.get("wbgpager_next_text", "&gt;&gt;")
-
-    linkstyle = config.get("wbgpager_linkstyle", 1)
-    if linkstyle > 1: linkstyle = 1
-
-    # grab the entry list
-    entry_list = data["entry_list"]
-    max = config.get("wbgpager_num_entries", 20)
-
-    page(request, config.get("wbgpager_num_entries", 20), entry_list)
